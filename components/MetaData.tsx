d896084873f8569f9b1ce55746068158379fb41c
Vioswift <kylewww@gmail.com>
2023-01-23 11:20:38 +1100

    Updated meta data

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -1,6 +1,6 @@
 export default {
     name: "Kyle Maurer",
-    title: "Kyle Maurer - Frontend/Backend/DevOps Engineer",
+    title: "Kyle Maurer - Frontend/Backend/Cloud/DevOps Engineer",
     description: "I am a talented Front-End/Back-end developer with a DevOps and Infrastructure Skills.",
-    keywords: "ReactJS, NextJS, DevOps, C#, TypeScript, IaC, Front-end, Back-end"
+    keywords: "ReactJS, NextJS, DevOps, Cloud, C#, TypeScript, IaC, Front-end, Back-end"
 }
