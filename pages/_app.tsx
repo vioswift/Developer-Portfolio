c4b0707a0b174df338d2fcdf254d14971da9357f
Vioswift <46578570+vioswift@users.noreply.github.com>
2023-02-06 18:27:54 +1100

    added more favicons and did a code clean-up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import "@fortawesome/fontawesome-svg-core/styles.css"; 
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false; 
+// import "@fortawesome/fontawesome-svg-core/styles.css"; 
+// import { config } from "@fortawesome/fontawesome-svg-core";
+// config.autoAddCss = false; 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,13 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>{SiteMetadata.title}</title>
+
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
+
         <meta name="author" content={SiteMetadata.name} />
         <meta name="keywords" content={SiteMetadata.keywords} />
         <meta name="description" content={SiteMetadata.description} />
